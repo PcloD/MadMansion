dd70b354876a963de00210c3ef1a851226dae15d
Julian Ceipek <julian.ceipek@gmail.com>
2014-11-11 21:05:19 -0800

    Implemented ghost and hunter movement

---
+++ MadMansion/Assets/PlayerInputManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerInputManager : MonoBehaviour {
+
+	public static PlayerInputManager g;
+
+	private InputDevice _hunter = null;
+	private InputDevice _ghost = null;
+
+	public InputDevice Hunter {
+		get { return _hunter; }
+	}
+
+	public InputDevice Ghost {
+		get { return _ghost; }
+	}
+
+	private enum PlayerSelectionStatus {
+		AssigningHunter,
+		AssigningGhost,
+		AllAssigned
+	}
+
+	private PlayerSelectionStatus _selectionStatus = PlayerSelectionStatus.AssigningHunter;
+
+	void Awake () {
+		if (g == null) {
+			g = this;
+
+			InputManager.OnActiveDeviceChanged += inputDevice => SetupController(inputDevice);
+		} else {
+			Destroy(this);
+		}
+	}
+
+	private void SetupController(InputDevice controller) {
+		switch (_selectionStatus) {
+			case PlayerSelectionStatus.AssigningHunter:
+				Debug.Log("Assigning Hunter: " + controller.Name);
+				_hunter = controller;
+				_selectionStatus = PlayerSelectionStatus.AssigningGhost;
+				break;
+			case PlayerSelectionStatus.AssigningGhost:
+				Debug.Log("Assigning Ghost: " + controller.Name);
+				_ghost = controller;
+				_selectionStatus = PlayerSelectionStatus.AllAssigned;
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ MadMansion/Assets/Scripts/GhostController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class GhostController : MonoBehaviour {
+
+	[SerializeField]
+	private float _movementSpeed = 10f;
+	[SerializeField]
+	private float _rotationSpeed = 10f;
+	[SerializeField]
+	private float _rotationSensitivity = 0.5f;
+
+	private Vector3 _inputVector;
+	private Rigidbody _rigidbody;
+	private Transform _transform;
+
+	void Awake () {
+		_rigidbody = GetComponent<Rigidbody>();
+		_transform = transform;
+	}
+
+	void Update () {
+		InputDevice device = PlayerInputManager.g.Ghost;
+
+		if (device != null) {
+			_inputVector = new Vector3(device.LeftStickX.Value, 0f, device.LeftStickY.Value);
+		}
+
+		if (_rigidbody.velocity.sqrMagnitude > _rotationSensitivity) {
+			transform.forward = Vector3.RotateTowards(transform.forward, _rigidbody.velocity, Time.deltaTime * _rotationSpeed, 0.0001f);
+		}
+	}
+
+	void FixedUpdate () {
+		var relativeVelocity = _inputVector * _movementSpeed;
+
+		// Calcualte the delta velocity
+		var currRelativeVelocity = rigidbody.velocity;
+		var velocityChange = relativeVelocity - currRelativeVelocity;
+
+		_rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+	}
+}
