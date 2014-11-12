fea0a40ca68cf235a475a3f759e419d1ed90daee
Julian Ceipek <julian.ceipek@gmail.com>
2014-11-11 21:36:32 -0800

    Refactoring; enabling possession of the characters.

---
+++ MadMansion/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public enum ControlPriority {
+	Ghost,
+	Hunter,
+	NPC
+}
+
+public class CharacterMotor : MonoBehaviour {
+
+	[SerializeField]
+	private float _movementSpeed = 7f;
+	[SerializeField]
+	private float _rotationSpeed = 10f;
+	[SerializeField]
+	private float _rotationSensitivity = 0.5f;
+
+	[SerializeField]
+	private float _ghostMovementSensitivity = 0.5f;
+
+	private GhostController _ghostController;
+
+	private Vector3 _ghostInputVector;
+	private Vector3 _standardInputVector;
+	private Rigidbody _rigidbody;
+	private Transform _transform;
+
+	public bool IsPossessed {
+		get { return _ghostController.enabled; }
+	}
+
+	public void AddInputWithPriority (Vector3 input, ControlPriority priority) {
+		input.y = 0f;
+		switch (priority) {
+			case ControlPriority.Ghost:
+				_ghostInputVector = input;
+				break;
+			default:
+				_standardInputVector = input;
+				break;
+		}
+	}
+
+	void Awake () {
+		_rigidbody = GetComponent<Rigidbody>();
+		_ghostController = GetComponent<GhostController>();
+		_transform = transform;
+	}
+
+	void Update () {
+		// Rotate towards velocity vector
+		if (_rigidbody.velocity.sqrMagnitude > _rotationSensitivity) {
+			_transform.forward = Vector3.RotateTowards(_transform.forward, _rigidbody.velocity, Time.deltaTime * _rotationSpeed, 0.0001f);
+		}
+	}
+
+	void FixedUpdate () {
+		Vector3 inputVector = _standardInputVector;
+		if (IsPossessed && _ghostInputVector.sqrMagnitude > _ghostMovementSensitivity) {
+			inputVector = _ghostInputVector;
+		}
+		var relativeVelocity = inputVector * _movementSpeed;
+
+		// Calcualte the delta velocity
+		var currRelativeVelocity = rigidbody.velocity;
+		var velocityChange = relativeVelocity - currRelativeVelocity;
+
+		_rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+	}
+}
--- MadMansion/Assets/Scripts/GhostController.cs
+++ MadMansion/Assets/Scripts/GhostController.cs
@@ -4,41 +4,17 @@ using InControl;
 
 public class GhostController : MonoBehaviour {
 
-	[SerializeField]
-	private float _movementSpeed = 10f;
-	[SerializeField]
-	private float _rotationSpeed = 10f;
-	[SerializeField]
-	private float _rotationSensitivity = 0.5f;
-
-	private Vector3 _inputVector;
-	private Rigidbody _rigidbody;
-	private Transform _transform;
+	private CharacterMotor _characterMotor;
 
 	void Awake () {
-		_rigidbody = GetComponent<Rigidbody>();
-		_transform = transform;
+		_characterMotor = GetComponent<CharacterMotor>();
 	}
 
 	void Update () {
 		InputDevice device = PlayerInputManager.g.Ghost;
-
 		if (device != null) {
-			_inputVector = new Vector3(device.LeftStickX.Value, 0f, device.LeftStickY.Value);
+			Vector3 inputVector = new Vector3(device.LeftStickX.Value, 0f, device.LeftStickY.Value);
+			_characterMotor.AddInputWithPriority(inputVector, ControlPriority.Ghost);
 		}
-
-		if (_rigidbody.velocity.sqrMagnitude > _rotationSensitivity) {
-			transform.forward = Vector3.RotateTowards(transform.forward, _rigidbody.velocity, Time.deltaTime * _rotationSpeed, 0.0001f);
-		}
-	}
-
-	void FixedUpdate () {
-		var relativeVelocity = _inputVector * _movementSpeed;
-
-		// Calcualte the delta velocity
-		var currRelativeVelocity = rigidbody.velocity;
-		var velocityChange = relativeVelocity - currRelativeVelocity;
-
-		_rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 	}
 }
