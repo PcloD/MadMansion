365c83f1ab7548a300ee4404afd244fa886f91e6
Julian Ceipek <julian.ceipek@gmail.com>
2014-11-20 18:47:05 -0800

    Adding infrastructure for UI.

---
+++ MadMansion/Assets/Scripts/HauntUI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HauntUI : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _hauntIconPrefab;
+	[SerializeField]
+	private float _separation = 1f;
+	[SerializeField]
+	private Renderer _radialProgressRenderer;
+
+	private int _displayedHauntCount;
+	private List<GameObject> _hauntIcons = new List<GameObject>();
+	private Transform _currTransform;
+
+	void Start () {
+		_currTransform = transform;
+	}
+
+	void Update () {
+		UpdateDisplayedHaunts();
+		UpdateRadialTimer();
+	}
+
+	private void UpdateRadialTimer () {
+		_radialProgressRenderer.material.SetFloat ("_Cutoff", 1f - HauntManager.g.HauntTimerPercentage);
+	}
+
+	private void UpdateDisplayedHaunts () {
+		while (_displayedHauntCount < HauntManager.g.HauntCount) {
+			Vector3 pos = _currTransform.position + transform.right * _separation;
+			GameObject newHauntIcon = Instantiate(_hauntIconPrefab, pos, _currTransform.rotation) as GameObject;
+			_currTransform = newHauntIcon.transform;
+			_hauntIcons.Add(newHauntIcon);
+			_displayedHauntCount++;
+		}
+		while (_displayedHauntCount > HauntManager.g.HauntCount) {
+			Destroy(_hauntIcons[_displayedHauntCount - 1]);
+			_hauntIcons.RemoveAt(_displayedHauntCount - 1);
+			_displayedHauntCount--;
+			if (_displayedHauntCount > 0) {
+				_currTransform = _hauntIcons[_displayedHauntCount - 1].transform;
+			} else {
+				_currTransform = transform;
+			}
+		}
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position + transform.right * _separation, 0.3f);
+	}
+}
---
+++ MadMansion/Assets/Scripts/Static/HauntManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HauntManager : MonoBehaviour {
+
+	public static HauntManager g;
+	[SerializeField]
+	private float _hauntCooldown = 4f;
+	[SerializeField]
+	private float _hauntDuration = 4f;
+	[SerializeField] // XXX: TODO: make this implicit
+	private float _hauntTimerPercentage = 0.5f;
+	public float HauntTimerPercentage {
+		get { return _hauntTimerPercentage; }
+	}
+	[SerializeField]
+	private int _hauntCount = 4;
+	public int HauntCount {
+		get { return _hauntCount; }
+	}
+
+	void Awake () {
+		if (g == null) {
+			g = this;
+		} else {
+			Destroy(this);
+		}
+	}
+
+}
