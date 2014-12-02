74bc4acba9c7f8a5dcd36a76e064fd1d06a87534
Julian Ceipek <julian.ceipek@gmail.com>
2014-12-02 00:22:00 -0800

    Added TV video and made fridge open and close

---
+++ MadMansion/Assets/FridgeFurniture.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FridgeFurniture : MonoBehaviour, IFurniture {
+
+    public Vector3 Position {
+    	get { return transform.position; }
+    }
+
+    [SerializeField]
+    private Transform _doorTransform;
+    [SerializeField]
+    private float _duration = 1f;
+    private bool _open = false;
+    private int _visitors = 0;
+    private Quaternion _initialRotation;
+    void Start () {
+        _initialRotation = _doorTransform.rotation;
+    }
+
+    void OnTriggerEnter (Collider other) {
+        CharacterMotor character = other.GetComponent<CharacterMotor>();
+        if (character != null) {
+            Debug.Log("OPEN");
+            _visitors++;
+            if (!_open && _visitors == 1) {
+                Open();
+            }
+        }
+    }
+
+    void OnTriggerExit (Collider other) {
+        CharacterMotor character = other.GetComponent<CharacterMotor>();
+        if (character != null) {
+            _visitors--;
+            if (_open && _visitors == 0) {
+                Close();
+            }
+        }
+    }
+
+    private void Open () {
+        _open = true;
+        StartCoroutine(BeginOpening());
+    }
+
+    private void Close () {
+        _open = false;
+        StartCoroutine(BeginClosing());
+    }
+
+    private IEnumerator BeginOpening () {
+        Quaternion open = Quaternion.Euler(0, 80, 0);
+        YieldInstruction wait = new WaitForFixedUpdate();
+        float counter = 0f;
+        while (counter < _duration) {
+            counter += Time.fixedDeltaTime;
+            _doorTransform.rotation = Quaternion.Lerp(_initialRotation, open, counter/_duration);
+            yield return wait;
+        }
+    }
+
+    private IEnumerator BeginClosing () {
+        Quaternion openRotation = Quaternion.Euler(0, 80, 0);
+        YieldInstruction wait = new WaitForFixedUpdate();
+        float counter = 0f;
+        while (counter < _duration) {
+            counter += Time.fixedDeltaTime;
+            _doorTransform.rotation = Quaternion.Lerp(openRotation, _initialRotation, counter/_duration);
+            yield return wait;
+        }
+    }
+
+}
---
+++ MadMansion/Assets/TVFurniture.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TVFurniture : MonoBehaviour, IFurniture {
+	[SerializeField]
+    private int _columns = 37;
+    [SerializeField]
+    private int _rows = 37;
+    [SerializeField]
+    private float _framesPerSecond = 12f;
+    [SerializeField]
+    private int _materialIndex = 0;
+    [SerializeField]
+    private Renderer _renderer;
+    [SerializeField]
+    private int _totalFrames = 100;
+
+    //the current frame to display
+    private int index = 0;
+
+    public Vector3 Position {
+    	get { return transform.position; }
+    }
+
+    void Start()
+    {
+        StartCoroutine(UpdateTiling());
+
+        //set the tile size of the texture (in UV units), based on the _rows and _columns
+        Vector2 size = new Vector2(1f / _columns, 1f / _rows);
+        _renderer.materials[_materialIndex].SetTextureScale("_MainTex", size);
+    }
+
+    private IEnumerator UpdateTiling()
+    {
+        while (true) {
+            //move to the next index
+            index++;
+            if (index >= _totalFrames)
+                index = 0;
+
+            //split into x and y indexes
+            Vector2 offset = new Vector2((float)index / _columns - (index / _columns), //x index
+                                          (index / _columns) / (float)_rows);          //y index
+
+            _renderer.materials[_materialIndex].SetTextureOffset("_MainTex", offset);
+
+            yield return new WaitForSeconds(1f / _framesPerSecond);
+        }
+
+    }
+}
