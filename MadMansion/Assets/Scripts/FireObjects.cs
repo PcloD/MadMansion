ddcdde0e90362f54babe93d8010abaaffd750e07
georgeli <georgeli1025@gmail.com>
2014-12-01 17:04:49 -0800

    Added fire

---
+++ MadMansion/Assets/Scripts/FireObjects.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireObjects : MonoBehaviour {
+	[SerializeField]
+	private GameObject _fireParticlePrefab;
+	[SerializeField]
+	private float speedY=0.5f;
+	[SerializeField]
+	private float timeIntervel=1f;
+	[SerializeField]
+	private int firstCreate=40;
+	[SerializeField]
+	private int maxCount=100;
+	[SerializeField]
+	private float maxYPos=3f;
+	[SerializeField]
+	private float posRange=1f;
+	private Queue<Transform> existingParticles = new Queue<Transform>();
+	private Queue<Transform> nonexistingParticles = new Queue<Transform>();
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnNewParticle());
+	}
+	void Awake(){
+		for (int index=0; firstCreate<20; index++ )
+		{
+			Transform newTransform=UnusedParticle;
+		}
+	}
+
+	private Transform UnusedParticle {
+		get {
+			Vector3 randomPos = new Vector3(Random.Range(-0.3f,0.3f), 0,Random.Range(-0.3f,0.3f)) + transform.position;
+			if (nonexistingParticles.Count > 0) {
+				Transform reusedFireTransform = nonexistingParticles.Dequeue(); 
+				existingParticles.Enqueue(reusedFireTransform);
+				reusedFireTransform.gameObject.SetActive(true);
+				reusedFireTransform.position = randomPos;
+				return reusedFireTransform;
+			} else {
+				GameObject newFireParticleObject = Instantiate(_fireParticlePrefab, randomPos, Quaternion.identity) as GameObject;
+				Transform newFireTransform = newFireParticleObject.transform;
+				existingParticles.Enqueue(newFireTransform);
+				return newFireTransform;
+			}
+		}
+	}
+
+	private void CleanupOldest () {
+		Transform oldest = existingParticles.Dequeue();
+		oldest.gameObject.SetActive(false);
+		nonexistingParticles.Enqueue(oldest);
+	}
+
+	private void MoveParticleInOneFrame(Transform T){
+		T.localScale=T.localScale*1f;
+		T.position=T.position+new Vector3(Random.Range(-posRange,posRange),speedY,Random.Range(-posRange,posRange)) * Time.deltaTime;
+		/*if (T.position.x>0.3f)
+		{
+			T.position=new Vector3(0.2f+Random.Range(-0.03f,0.03f),T.position.y,T.position.z);
+		}
+		else if (T.position.x<-0.3f)
+		{
+			T.position=new Vector3(-0.2f+Random.Range(-0.03f,0.03f),T.position.y,T.position.z);
+		}
+		if (T.position.z>0.3f)
+		{
+			T.position=new Vector3(T.position.x,T.position.y,0.2f+Random.Range(-0.03f,0.03f));
+		}
+		else if (T.position.z<-0.3f)
+		{
+			T.position=new Vector3(T.position.x,T.position.y,-0.2f+Random.Range(-0.03f,0.03f));
+		}*/
+		if (T.position.y>maxYPos)
+		{
+			CleanupOldest();
+		}
+	}
+
+	private IEnumerator SpawnNewParticle () {
+		while (true) {
+			yield return new WaitForSeconds(timeIntervel);
+			Transform newTransform = UnusedParticle;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//move
+		foreach (Transform F in existingParticles)
+		{
+			MoveParticleInOneFrame(F);
+
+		}
+			
+		while (existingParticles.Count > maxCount)//a particle reaches the end of its life
+		{
+			CleanupOldest();
+		}
+	}
+}
---
+++ MadMansion/Assets/Scripts/FireParticle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireParticle : MonoBehaviour {
+	[SerializeField]
+	private GameObject _fireParticlePrefab;
+	private ParticleSystem _particleSystem;
+	private ParticleSystem.Particle[] _allParticles = new ParticleSystem.Particle[100];
+	private Dictionary<ParticleSystem.Particle,Transform> _particleMap = new Dictionary<ParticleSystem.Particle,Transform>();
+	// Use this for initialization
+	void Start () {
+
+		//var FireParticleSystem=this.GetComponent<Particle System>;
+	}
+	void Awake(){
+		_particleSystem=GetComponent<ParticleSystem>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//int num=ParticleSystem.getParticles(FireParticleSystem);
+
+		int particleCount = _particleSystem.GetParticles(_allParticles);
+		
+		for(int index = 0; index < particleCount; index++){
+			ParticleSystem.Particle p = _allParticles[index];
+
+			if (!_particleMap.ContainsKey(p))
+			{
+				GameObject newFireParticle = Instantiate(_fireParticlePrefab, p.position, Quaternion.identity) as GameObject;
+				_particleMap[p] = newFireParticle.transform;
+			}
+			else
+			{
+				_particleMap[p].position = p.position;
+			}
+			//work your magic
+		}
+	
+	}
+
+}
