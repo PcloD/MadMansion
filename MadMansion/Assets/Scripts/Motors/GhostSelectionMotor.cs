4a938614b8843be23b9ebf89cd3d5a96431c9a9a
georgeli <georgeli1025@gmail.com>
2015-02-06 18:03:29 -0800

    Cancle the magic circle after catching wrong. Change the ghostSelection GameObject out of Initialize and Destroy it when getting the message of catch wrong

--- MadMansion/Assets/Scripts/Motors/GhostSelectionMotor.cs
+++ MadMansion/Assets/Scripts/Motors/GhostSelectionMotor.cs
@@ -5,109 +5,136 @@ using System.Diagnostics;
 
 [RequireComponent (typeof(CurrRoomFinder))]
 [RequireComponent (typeof(CharacterMotor))]
-public class GhostSelectionMotor : MonoBehaviour {
+public class GhostSelectionMotor : MonoBehaviour
+{
 
-	[SerializeField]
-	private GameObject _ghostSelectionCirclePrefab;
-	[SerializeField]
-	private float _selectionSensitivity = 0.8f;
-	[SerializeField]
-	private float _inputDelay = 0.3f;
+		[SerializeField]
+		private GameObject
+				_ghostSelectionCirclePrefab;
+		[SerializeField]
+		private float
+				_selectionSensitivity = 0.8f;
+		[SerializeField]
+		private float
+				_inputDelay = 0.3f;
 
-	private Transform _ghostSelectionCircleTransform = null;
-	private Vector3 _oldPos = Vector3.zero;
-	private Transform _targetTransform;
-	private Transform _transform;
-	private CurrRoomFinder _currRoomFinder;
-	private List<Transform> _sortedTransformsInRoom = new List<Transform>();
-	private int _currSelection = 0;
-	private CharacterMotor _characterMotor;
+		private Transform _ghostSelectionCircleTransform = null;
 
-	void Awake () {
-		_transform = transform;
-		_currRoomFinder = GetComponent<CurrRoomFinder>();
-		_characterMotor = GetComponent<CharacterMotor>();
-	}
+		GameObject ghostSelectionCircle;
 
-	public void Initialize () {
-	 	GameObject ghostSelectionCircle;
-		ghostSelectionCircle = Instantiate(_ghostSelectionCirclePrefab, _transform.position, Quaternion.identity) as GameObject;
-		_ghostSelectionCircleTransform = ghostSelectionCircle.transform;
-		RoomManager.g.DimOtherRooms(exclude: _currRoomFinder.Room);
-		HashSet<CharacterMotor> peopleInRoom = _currRoomFinder.Room.Characters;
-		_targetTransform = _transform;
-		_oldPos = _targetTransform.position;
+		private Vector3 _oldPos = Vector3.zero;
+		private Transform _targetTransform;
+		private Transform _transform;
+		private CurrRoomFinder _currRoomFinder;
+		private List<Transform> _sortedTransformsInRoom = new List<Transform> ();
+		private int _currSelection = 0;
+		private CharacterMotor _characterMotor;
 
-		HashSet<Transform> exclude = new HashSet<Transform>();
-		Transform currTransform = _transform;
-		_sortedTransformsInRoom.Add (currTransform);
-		exclude.Add(currTransform);
-		while (exclude.Count < peopleInRoom.Count) {
-			currTransform = FindClosestTransformNotInSet(currTransform.position, peopleInRoom, exclude);
-			_sortedTransformsInRoom.Add (currTransform);
-			exclude.Add(currTransform);
+		void Awake ()
+		{
+				_transform = transform;
+				_currRoomFinder = GetComponent<CurrRoomFinder> ();
+				_characterMotor = GetComponent<CharacterMotor> ();
 		}
-	}
+		void OnEnable ()
+		{
+				Events.g.AddListener<CatchWrongEvent> (StopSelection);
+		}
+		void OnDisable ()
+		{
 
-	public Transform FindClosestTransformNotInSet(Vector3 target, HashSet<CharacterMotor> allCharacters, HashSet<Transform> exclude) {
-		float minimumSqrDistance = Mathf.Infinity;
-		Transform closest = null;
-		foreach (CharacterMotor character in allCharacters) {
-			Transform otherTransform = character.transform;
-			Vector3 otherPos = otherTransform.position;
-			Vector3 toOther = otherPos - target;
-			float sqrDist = toOther.sqrMagnitude;
-			if (sqrDist < minimumSqrDistance && !exclude.Contains(otherTransform)) {
-				closest = otherTransform;
-				minimumSqrDistance = sqrDist;
-			}
+				Events.g.RemoveListener<CatchWrongEvent> (StopSelection);
 		}
 
-		return closest;
-	}
+		public void Initialize ()
+		{
+				
+				ghostSelectionCircle = Instantiate (_ghostSelectionCirclePrefab, _transform.position, Quaternion.identity) as GameObject;
+				_ghostSelectionCircleTransform = ghostSelectionCircle.transform;
+				RoomManager.g.DimOtherRooms (exclude: _currRoomFinder.Room);
+				HashSet<CharacterMotor> peopleInRoom = _currRoomFinder.Room.Characters;
+				_targetTransform = _transform;
+				_oldPos = _targetTransform.position;
 
-	private int Modulo (int x, int m)
-	{
-		return x < 0 ? ((x % m) + m) % m : x % m;
-	}
+				HashSet<Transform> exclude = new HashSet<Transform> ();
+				Transform currTransform = _transform;
+				_sortedTransformsInRoom.Add (currTransform);
+				exclude.Add (currTransform);
+				while (exclude.Count < peopleInRoom.Count) {
+						currTransform = FindClosestTransformNotInSet (currTransform.position, peopleInRoom, exclude);
+						_sortedTransformsInRoom.Add (currTransform);
+						exclude.Add (currTransform);
+				}
+		}
 
-	private Stopwatch _inputDelayer = new Stopwatch();
-	public void AddInput(Vector3 inputVector) {
-		if (_inputDelayer.ElapsedMilliseconds >= _inputDelay * 1000f) {
-			_inputDelayer.Reset();
-			_inputDelayer.Stop();
-		}
-		if (_inputDelayer.IsRunning || inputVector.sqrMagnitude < _selectionSensitivity) {
-			return;
-		}
-		int delta = -1;
-		Vector3 upRight = (new Vector3(1f,0f,1f)).normalized;
-		Vector3 bottomLeft = (new Vector3(-1f,0f,-1f)).normalized;
-		if (Vector3.Dot(inputVector, upRight) > Vector3.Dot(inputVector, bottomLeft)) {
-			delta = 1;
-			_inputDelayer.Reset();
-			_inputDelayer.Start();
-		}
-		_currSelection = Modulo(_currSelection + delta, _sortedTransformsInRoom.Count);
-		if (_targetTransform != null) {
-			_targetTransform.position = _oldPos;
-		}
-		_targetTransform = _sortedTransformsInRoom[_currSelection];
-		_oldPos = _targetTransform.position;
-		_inputDelayer.Reset();
-		_inputDelayer.Start();
-	}
+		public Transform FindClosestTransformNotInSet (Vector3 target, HashSet<CharacterMotor> allCharacters, HashSet<Transform> exclude)
+		{
+				float minimumSqrDistance = Mathf.Infinity;
+				Transform closest = null;
+				foreach (CharacterMotor character in allCharacters) {
+						Transform otherTransform = character.transform;
+						Vector3 otherPos = otherTransform.position;
+						Vector3 toOther = otherPos - target;
+						float sqrDist = toOther.sqrMagnitude;
+						if (sqrDist < minimumSqrDistance && !exclude.Contains (otherTransform)) {
+								closest = otherTransform;
+								minimumSqrDistance = sqrDist;
+						}
+				}
 
-	void LateUpdate () {
-		if (_ghostSelectionCircleTransform != null && _targetTransform != null) {
-			_ghostSelectionCircleTransform.position = _targetTransform.position;
-			if (_inputDelayer.IsRunning) {
-				_targetTransform.position = _oldPos + Vector3.up * 3f * _inputDelayer.ElapsedMilliseconds/1000f;
-			}
+				return closest;
 		}
-	}
 
-	public void FinalizeCatch () {
-		Events.g.Raise(new FinishCatchEvent(hunter: _characterMotor, guess: _targetTransform.GetComponent<CharacterMotor>()));
-	}
+		private int Modulo (int x, int m)
+		{
+				return x < 0 ? ((x % m) + m) % m : x % m;
+		}
+
+		private Stopwatch _inputDelayer = new Stopwatch ();
+		public void AddInput (Vector3 inputVector)
+		{
+				if (_inputDelayer.ElapsedMilliseconds >= _inputDelay * 1000f) {
+						_inputDelayer.Reset ();
+						_inputDelayer.Stop ();
+				}
+				if (_inputDelayer.IsRunning || inputVector.sqrMagnitude < _selectionSensitivity) {
+						return;
+				}
+				int delta = -1;
+				Vector3 upRight = (new Vector3 (1f, 0f, 1f)).normalized;
+				Vector3 bottomLeft = (new Vector3 (-1f, 0f, -1f)).normalized;
+				if (Vector3.Dot (inputVector, upRight) > Vector3.Dot (inputVector, bottomLeft)) {
+						delta = 1;
+						_inputDelayer.Reset ();
+						_inputDelayer.Start ();
+				}
+				_currSelection = Modulo (_currSelection + delta, _sortedTransformsInRoom.Count);
+				if (_targetTransform != null) {
+						_targetTransform.position = _oldPos;
+				}
+				_targetTransform = _sortedTransformsInRoom [_currSelection];
+				_oldPos = _targetTransform.position;
+				_inputDelayer.Reset ();
+				_inputDelayer.Start ();
+		}
+
+		void LateUpdate ()
+		{
+				if (_ghostSelectionCircleTransform != null && _targetTransform != null) {
+						_ghostSelectionCircleTransform.position = _targetTransform.position;
+						if (_inputDelayer.IsRunning) {
+								_targetTransform.position = _oldPos + Vector3.up * 3f * _inputDelayer.ElapsedMilliseconds / 1000f;
+						}
+				}
+		}
+
+		void StopSelection (CatchWrongEvent e)
+		{
+				Destroy (ghostSelectionCircle);
+		}
+
+		public void FinalizeCatch ()
+		{
+				Events.g.Raise (new FinishCatchEvent (hunter: _characterMotor, guess: _targetTransform.GetComponent<CharacterMotor> ()));
+		}
 }
