c8059a0b6117e4246f646e961e64316e4751bfca
Julian Ceipek <julian.ceipek@gmail.com>
2015-04-11 16:23:40 -0700

    Removed exit and catch not end

---
+++ MadMansion/Assets/RestarterHack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestarterHack : MonoBehaviour {
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Events.g.Raise(new RestartGameEvent());
+		}
+	}
+}
--- MadMansion/Assets/Scripts/Static/CatchManager.cs
+++ MadMansion/Assets/Scripts/Static/CatchManager.cs
@@ -5,135 +5,135 @@ using System.Diagnostics;
 public class CatchManager : MonoBehaviour
 {
 
-	public static CatchManager g;
-	[SerializeField]
-	private float
-		_catchChargeDuration = 4f;
-	public float CatchChargePercentage {
-		get {
-			return Mathf.Min (1f, _catchChargeTimer.ElapsedMilliseconds / (_catchChargeDuration * 1000f));
+		public static CatchManager g;
+		[SerializeField]
+		private float
+				_catchChargeDuration = 4f;
+		public float CatchChargePercentage {
+				get {
+						return Mathf.Min (1f, _catchChargeTimer.ElapsedMilliseconds / (_catchChargeDuration * 1000f));
+				}
 		}
-	}
 
-	private Stopwatch _catchChargeTimer = new Stopwatch ();
-	private bool _catchChargeTimerPaused = false;
-	private bool _catchChargeTimerIsRunning {
-		get { return _catchChargeTimerPaused || _catchChargeTimer.IsRunning; }
-	}
-
-	void Update ()
-	{
-
-	}
-
-	void Awake ()
-	{
-		if (g == null) {
-			g = this;
-		} else {
-			Destroy (this);
+		private Stopwatch _catchChargeTimer = new Stopwatch ();
+		private bool _catchChargeTimerPaused = false;
+		private bool _catchChargeTimerIsRunning {
+				get { return _catchChargeTimerPaused || _catchChargeTimer.IsRunning; }
 		}
-	}
 
-	void OnEnable ()
-	{
-		Events.g.AddListener<StartGameEvent> (BeginCharging);
-		Events.g.AddListener<PauseGameEvent> (PauseTimers);
-		Events.g.AddListener<ResumeGameEvent> (ResumeTimers);
-		Events.g.AddListener<CatchEvent> (StartCatching);
-		Events.g.AddListener<FinishCatchEvent> (FinalizeCatching);
-		Events.g.AddListener<CatchEffectEndEvent> (StopCatching);
-	}
-
-	void OnDisable ()
-	{
-		Events.g.RemoveListener<PauseGameEvent> (PauseTimers);
-		Events.g.RemoveListener<ResumeGameEvent> (ResumeTimers);
-		Events.g.RemoveListener<StartGameEvent> (BeginCharging);
-		Events.g.RemoveListener<CatchEvent> (StartCatching);
-		Events.g.RemoveListener<FinishCatchEvent> (FinalizeCatching);
-		Events.g.RemoveListener<CatchEffectEndEvent> (StopCatching);
-	}
-
-	private void PauseTimers (PauseGameEvent e)
-	{
-		if (_catchChargeTimer.IsRunning) {
-			_catchChargeTimer.Stop ();
-
-			_catchChargeTimerPaused = true;
+		void Update ()
+		{
 
 		}
-	}
 
-	private void ResumeTimers (ResumeGameEvent e)
-	{
-		if (_catchChargeTimerPaused) {
-			_catchChargeTimer.Start ();
-
-			_catchChargeTimerPaused = false;
+		void Awake ()
+		{
+				if (g == null) {
+						g = this;
+				} else {
+						Destroy (this);
+				}
 		}
-	}
 
-	private void BeginCharging (StartGameEvent e)
-	{
-		// Handle event here
-		StartCatchCharge ();
-	}
-
-	private void StartCatching (CatchEvent e)
-	{
-		if (e.successful) {
-			print ("Succeeded catch event");
-			TimeManager.g.StartBulletTime ();
-			_isCatching = true;
-		} else {
-			print ("Failed catch event");
+		void OnEnable ()
+		{
+				Events.g.AddListener<StartGameEvent> (BeginCharging);
+				Events.g.AddListener<PauseGameEvent> (PauseTimers);
+				Events.g.AddListener<ResumeGameEvent> (ResumeTimers);
+				Events.g.AddListener<CatchEvent> (StartCatching);
+				Events.g.AddListener<FinishCatchEvent> (FinalizeCatching);
+//				Events.g.AddListener<CatchEffectEndEvent> (StopCatching);
 		}
-	}
 
-	private void StopCatching (CatchEffectEndEvent e)
-	{
-		TimeManager.g.StopBulletTime ();
-		_isCatching = false;
-		_catchChargeTimer.Reset ();
-		StartCatchCharge ();
-
-	}
-
-	private void FinalizeCatching (FinishCatchEvent e)
-	{
-		if (e.guess == null) {
-			return;
+		void OnDisable ()
+		{
+				Events.g.RemoveListener<PauseGameEvent> (PauseTimers);
+				Events.g.RemoveListener<ResumeGameEvent> (ResumeTimers);
+				Events.g.RemoveListener<StartGameEvent> (BeginCharging);
+				Events.g.RemoveListener<CatchEvent> (StartCatching);
+				Events.g.RemoveListener<FinishCatchEvent> (FinalizeCatching);
+//				Events.g.RemoveListener<CatchEffectEndEvent> (StopCatching);
 		}
-		GhostController ghostController = e.guess.GetComponent<GhostController> ();
-		if (ghostController != null && ghostController.enabled) {//catch right
-			Events.g.Raise (new CatchEndEvent (true));
-			if (e.hunter == e.guess) {
-				Events.g.Raise (new EndGameEvent (winner: Player.NoPlayer, rationale: EndReason.HunterCaughtGhostInSameBody));
-			} else {
-				Events.g.Raise (new EndGameEvent (winner: Player.HunterPlayer, rationale: EndReason.HunterCaughtGhost));
-			}
-		} else {
-			//Events.g.Raise(new EndGameEvent(winner: Player.GhostPlayer, rationale: EndReason.HunterCaughtInnocent));
-			//StopCatching ();
-			Events.g.Raise (new CatchEndEvent (false));
+
+		private void PauseTimers (PauseGameEvent e)
+		{
+				if (_catchChargeTimer.IsRunning) {
+						_catchChargeTimer.Stop ();
+
+						_catchChargeTimerPaused = true;
+
+				}
 		}
-	}
+
+		private void ResumeTimers (ResumeGameEvent e)
+		{
+				if (_catchChargeTimerPaused) {
+						_catchChargeTimer.Start ();
+
+						_catchChargeTimerPaused = false;
+				}
+		}
+
+		private void BeginCharging (StartGameEvent e)
+		{
+				// Handle event here
+				StartCatchCharge ();
+		}
+
+		private void StartCatching (CatchEvent e)
+		{
+				if (e.successful) {
+						print ("Succeeded catch event");
+						TimeManager.g.StartBulletTime ();
+						_isCatching = true;
+				} else {
+						print ("Failed catch event");
+				}
+		}
+
+		private void StopCatching ()//CatchEffectEndEvent e)
+		{
+				TimeManager.g.StopBulletTime ();
+				_isCatching = false;
+				_catchChargeTimer.Reset ();
+				//StartCatchCharge ();
+
+		}
+
+		private void FinalizeCatching (FinishCatchEvent e)
+		{
+				if (e.guess == null) {
+						return;
+				}
+				GhostController ghostController = e.guess.GetComponent<GhostController> ();
+				if (ghostController != null && ghostController.enabled) {//catch right
+						Events.g.Raise (new CatchEndEvent (true));
+						if (e.hunter == e.guess) {
+								Events.g.Raise (new EndGameEvent (winner: Player.NoPlayer, rationale: EndReason.HunterCaughtGhostInSameBody));
+						} else {
+								Events.g.Raise (new EndGameEvent (winner: Player.HunterPlayer, rationale: EndReason.HunterCaughtGhost));
+						}
+				} else {
+						Events.g.Raise (new EndGameEvent (winner: Player.GhostPlayer, rationale: EndReason.HunterCaughtInnocent));
+						// StopCatching ();
+						// Events.g.Raise (new CatchEndEvent (false));
+				}
+		}
 
 
-	private bool _isCatching = false;
-	public bool IsCatching {
-		get { return _isCatching; }
-	}
+		private bool _isCatching = false;
+		public bool IsCatching {
+				get { return _isCatching; }
+		}
 
-	private void StartCatchCharge ()
-	{
-		_catchChargeTimer.Start ();
+		private void StartCatchCharge ()
+		{
+				_catchChargeTimer.Start ();
 
 
-	}
+		}
 
-	public bool CanCatch {
-		get { return _catchChargeTimerIsRunning && (_catchChargeTimer.ElapsedMilliseconds > _catchChargeDuration * 1000f); }
-	}
+		public bool CanCatch {
+				get { return _catchChargeTimerIsRunning && (_catchChargeTimer.ElapsedMilliseconds > _catchChargeDuration * 1000f); }
+		}
 }
